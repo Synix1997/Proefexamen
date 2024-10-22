0c4b4c825c1ed0b084d06f22acd7d7318c77ee7c
Synix1997 <deruyck.sebastien97@gmail.com>
2024-10-22 21:50:25 +0200

    Opdracht 2.8 - week_number afgewerkt

--- week_number.py
+++ week_number.py
@@ -3,13 +3,15 @@ from dow import day_of_the_week
 
 
 def week_number(year: int, month: int, day: int):
-    dow = day_of_the_week(year, month, day)
-    days = day_number(year, month, day)
-    week = ((dow - 2 + days) // 7) + 1
+    dow = day_of_the_week(year, month, day)  # Get the day of the week (1=Monday, ..., 7=Sunday)
+    days = day_number(year, month, day)  # Get the day number in the year
+    week = (days - dow) // 7 + 1  # Calculate the week number
     return week
 
 def main():
-    assert week_number(2022, 1, 1) == 1
+    assert week_number(2024, 1, 3) == 1
+    assert week_number(2024, 3, 16) == 11
+    assert week_number(2024, 12, 29) == 52
 
 
 if __name__ == "__main__":
