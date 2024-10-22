0300f6f3f21dc49b5fd91e19363c76c0ec0df949
Synix1997 <deruyck.sebastien97@gmail.com>
2024-10-22 21:38:03 +0200

    Geen aanpassing gedaan

--- dow.py
+++ dow.py
@@ -1,4 +1,8 @@
 def day_of_the_week(year: int, month: int, day: int) -> int:
+    """
+
+    :rtype: object
+    """
     offset = [0, 3, 2, 5, 0, 3, 5, 1, 4, 6, 2, 4]
     year -= month < 3
     dow = (year + year // 4 - year // 100 + year // 400 + offset[month-1] + day) % 7
