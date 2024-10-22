df2c3af3125b3e9ea104a6a60bdc134b852674af
Synix1997 <deruyck.sebastien97@gmail.com>
2024-10-22 21:38:36 +0200

    Opdracht 2.8 - week_number started with variables

---
+++ week_number.py
@@ -0,0 +1,16 @@
+from day_number import day_number
+from dow import day_of_the_week
+
+
+def week_number(year: int, month: int, day: int):
+    dow = day_of_the_week(year, month, day)
+    days = day_number(year, month, day)
+    week = ((dow - 2 + days) // 7) + 1
+    return week
+
+def main():
+    assert week_number(2022, 1, 1) == 1
+
+
+if __name__ == "__main__":
+    main()
